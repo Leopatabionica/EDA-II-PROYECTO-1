068c01c4c907dc5d6e20e3ae5d38a44fe2b37274
Leopatabionica <leorvantes@gmail.com>
2026-03-26 13:57:40 -0600

    Se implementa al algoritmo para un arreglo de números aleatorios, y diferentes tamaños para las pruebas de medición
    Allocate memory for an array and fill it with random integers before sorting.

--- QuickSort/PruebasQuicksort.c
+++ QuickSort/PruebasQuicksort.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+
+#include "Quicksort.h"
+
+int main()
+{
+    int tamaño = 100;
+
+    int *arreglo = (int *)malloc(tamaño * sizeof(int));
+
+    scrand(time(NULL));
+
+    for(int i = 0; i<tamaño; i++)
+    {
+        arreglo [i] = rand() % 2500;
+    }
+    int size = sizeof(arreglo)/( sizeof(arreglo[0]));
+
+    quicksort(arreglo, 0, size-1);
+    
+
+}
